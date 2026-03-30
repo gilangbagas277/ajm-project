23185a57def17ac878f534cf1d362856f16a555d
gilangbagas277 <gilangbagas140303@gmail.com>
2026-03-31 02:03:55 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -106,4 +106,8 @@ Terima kasih 🙏`
   }
 });
 
-app.listen(3000, () => console.log("Server running"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
